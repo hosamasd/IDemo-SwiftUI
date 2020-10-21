e470adf1714e77374be624ce8352a00dea8075ce
hosam <hosamelmalt@gmail.com>
2020-10-21 08:26:29 +0200

    Initial Commit

---
+++ IDemo/IDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDemoApp.swift
+//  IDemo
+//
+//  Created by hosam on 10/21/20.
+//
+
+import SwiftUI
+
+@main
+struct IDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
